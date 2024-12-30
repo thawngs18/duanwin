539ab0842d9af8538aee57f8916c072a81e6c066
Thắng Phan Đức <apgpdt10@gmail.com>
2024-12-30 10:12:19 +0700

    final

--- WindowsFormsApp8/GioiThieu.Designer.cs
+++ WindowsFormsApp8/GioiThieu.Designer.cs
@@ -40,9 +40,10 @@
             this.btn_login.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_login.ForeColor = System.Drawing.Color.White;
-            this.btn_login.Location = new System.Drawing.Point(326, 315);
+            this.btn_login.Location = new System.Drawing.Point(435, 388);
+            this.btn_login.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_login.Name = "btn_login";
-            this.btn_login.Size = new System.Drawing.Size(170, 46);
+            this.btn_login.Size = new System.Drawing.Size(227, 57);
             this.btn_login.TabIndex = 0;
             this.btn_login.Text = "Login";
             this.btn_login.UseVisualStyleBackColor = false;
@@ -52,42 +53,46 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe Print", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(104, 160);
+            this.label1.Location = new System.Drawing.Point(247, 197);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(652, 50);
+            this.label1.Size = new System.Drawing.Size(607, 64);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Chao mung moi nguoi den voi bua tiec cua";
+            this.label1.Text = "Chao mung moi nguoi den voi ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Ravie", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(269, 227);
+            this.label2.Location = new System.Drawing.Point(359, 279);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(287, 39);
+            this.label2.Size = new System.Drawing.Size(368, 50);
             this.label2.TabIndex = 1;
             this.label2.Text = "The Under Dog";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::WindowsFormsApp8.Properties.Resources.underdog;
-            this.pictureBox1.Location = new System.Drawing.Point(326, 26);
+            this.pictureBox1.Location = new System.Drawing.Point(435, 32);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(170, 131);
+            this.pictureBox1.Size = new System.Drawing.Size(227, 161);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
             // GioiThieu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_login);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "GioiThieu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "GioiThieu";
